f87754fea43282910e568fe5097ed3227f7f6242
Irina Krakavetskaya <irena.krakavetskaya@gmail.com>
2018-08-29 13:04:18 +0300

    initial commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+        $this->loadViewsFrom(__DIR__.'/resources/views/vendor/shop', 'courier');
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
---
+++ config/shop.php
@@ -0,0 +1,111 @@
+<?php
+
+return [
+
+	'routes' => [
+		 //'login' => ['middleware' => ['web']],
+		// 'jqadm' => ['prefix' => 'admin/{site}/jqadm', 'middleware' => ['web', 'auth']],
+		// 'extadm' => ['prefix' => 'admin/{site}/extadm', 'middleware' => ['web', 'auth']],
+		// 'jsonadm' => ['prefix' => 'admin/{site}/jsonadm', 'middleware' => ['web', 'auth']],
+		// 'jsonapi' => ['prefix' => 'jsonapi', 'middleware' => ['web', 'api']],
+		// 'account' => ['middleware' => ['web', 'auth']],
+		// 'default' => ['middleware' => ['web']],
+		// 'update' => [],
+	],
+
+	'page' => [
+        'account-index' => array('account/history'),
+		// 'account-index' => [ 'account/profile','account/history','account/favorite','account/watch','basket/mini','catalog/session' ],
+		// 'basket-index' => [ 'basket/standard','basket/related' ],
+		// 'catalog-count' => [ 'catalog/count' ],
+		// 'catalog-detail' => [ 'basket/mini','catalog/stage','catalog/detail','catalog/session' ],
+		  'catalog-list' => [ 'basket/mini','catalog/filter','catalog/stage','catalog/lists' ],
+		// 'catalog-stock' => [ 'catalog/stock' ],
+		// 'catalog-suggest' => [ 'catalog/suggest' ],
+		// 'checkout-confirm' => [ 'checkout/confirm' ],
+		// 'checkout-index' => [ 'checkout/standard' ],
+		// 'checkout-update' => [ 'checkout/update' ],
+	],
+
+	/*
+	'resource' => [
+		'db' => [
+			'adapter' => env('DB_CONNECTION', 'mysql'),
+			'host' => env('DB_HOST', 'localhost'),
+			'port' => env('DB_PORT', ''),
+			'socket' => '',
+			'database' => env('DB_DATABASE', 'laravel'),
+			'username' => env('DB_USERNAME', 'root'),
+			'password' => env('DB_PASSWORD', ''),
+			'stmt' => ["SET SESSION sort_buffer_size=2097144; SET NAMES 'utf8'; SET SESSION sql_mode='ANSI'"],
+		],
+	],
+	*/
+
+	'admin' => [],
+
+	'client' => [
+		'html' => [
+			'basket' => [
+				'cache' => [
+					// 'enable' => false, // Disable basket content caching
+				],
+			],
+			'common' => [
+				'content' => [
+					 'baseurl' => 'http://clans', //!!!
+				],
+				'template' => [
+					 'baseurl' => 'packages/aimeos/shop/themes/elegance',
+				],
+			],
+            'email' => [
+                'from-email' => 'irena@shop.by',
+                'from-name' => 'Irena',
+            ],
+		],
+	],
+
+	'controller' => [
+	],
+
+	'i18n' => [
+        'ru' => [
+            'admin' => [ //mshop, controller/frontend, client, controller/jobs, controller/extjs, admin
+                'customer' => ['покупатель'], //?
+            ],
+        ],
+	],
+
+	'madmin' => [
+		'cache' => [
+			'manager' => [
+				// 'name' => 'None', // Disable caching
+			],
+		],
+		'log' => [
+			'manager' => [
+				'standard' => [
+					// 'loglevel' => 7, // Enable debug logging into madmin_log table
+				],
+			],
+		],
+	],
+
+    'mshop' => [
+        'price'=> [
+            'taxflag' => 0,
+        ],
+    ],
+
+
+	'command' => [
+	],
+
+	'frontend' => [
+	],
+
+	'backend' => [
+	],
+
+];
